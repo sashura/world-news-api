f144ed9a41f7acfd32937eecc90d5c1c35d19fba
Alexandra Zakharova <alexandra.v.zakharova@gmail.com>
2020-05-26 21:36:24 +0300

    try cors

--- app.js
+++ app.js
@@ -32,30 +32,6 @@ app.use(bodyParser.json());
 
 app.use(requestLogger);
 
-/*const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'localhost:3000',
-  'localhost:8080',
-  'localhost:8081',
-  'http://localhost:8081',
-  'https://localhost:8081',
-  'http://localhost:8080',
-  'https://localhost:8080',
-];
-
-const corsOptionDelegate =
-
-app.use(function(req, res, next) {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-      res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  }
-  next();
-}); */
-
 app.use('/', router);
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
+const cors = require('cors');
 
 const users = require('./users');
 const articles = require('./articles');
 const wrongPage = require('./wrongPage');
 const authorization = require('./authorization');
 
-router.use('/articles', articles);
+router.use('/articles', cors(), articles);
 
 router.use('/users', users);
 
